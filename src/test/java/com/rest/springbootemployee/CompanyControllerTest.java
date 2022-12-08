cc474be3037630e8ed43337ef194e1f13ac15518
HoiDam <66233626+HoiDam@users.noreply.github.com>
2022-12-08 18:35:23 +0800

    feat: finished delete function in mongo

--- src/test/java/com/rest/springbootemployee/CompanyControllerTest.java
+++ src/test/java/com/rest/springbootemployee/CompanyControllerTest.java
@@ -152,7 +152,7 @@ public class CompanyControllerTest {
         employees.add(new Employee("lili", 20, "Female", 2000));
         employees.add(new Employee("coco", 10, "Female", 8000));
 
-        Company company = companyRepository.create(new Company("Spring", employees));
+        Company company = companyMongoRepository.save(new Company("Spring", employees));
 
         //when & then
         client.perform(MockMvcRequestBuilders.delete("/companies/{id}", company.getId()))
