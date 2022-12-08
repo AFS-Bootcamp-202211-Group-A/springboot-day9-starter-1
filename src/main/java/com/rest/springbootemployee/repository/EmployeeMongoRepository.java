b7287a9bf36bae03fc8741375a814af93b9a8ba3
Marie Chow <chowma3@oocl.com>
2022-12-08 15:39:30 +0800

    refactor: remove unused code

--- src/main/java/com/rest/springbootemployee/repository/EmployeeMongoRepository.java
+++ src/main/java/com/rest/springbootemployee/repository/EmployeeMongoRepository.java
@@ -3,15 +3,12 @@ package com.rest.springbootemployee.repository;
 import com.rest.springbootemployee.entity.Employee;
 import org.springframework.data.mongodb.repository.Aggregation;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Repository
 public interface EmployeeMongoRepository extends MongoRepository<Employee, String> {
-//    @Query("{'gender' : ?0}") <- turns out don't need this is ok too
     List<Employee> findByGender(String gender);
 
     @Aggregation(pipeline = {
