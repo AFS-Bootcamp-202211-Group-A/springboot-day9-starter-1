5170e042e317a860fbc864243c8da67835b26d3e
Alan Wan <alan.wan@oocl.com>
2022-12-08 15:15:06 +0800

    feat: implement update employee by mongo repo

--- src/test/java/com/rest/springbootemployee/EmployeeServiceTest.java
+++ src/test/java/com/rest/springbootemployee/EmployeeServiceTest.java
@@ -57,17 +57,17 @@ public class EmployeeServiceTest {
     @Test
     void should_update_only_age_and_salary_when_update_all_given_employees() {
         //given
-        int employeeId = 1;
+        String employeeId = "1";
         Employee employee = new Employee(new ObjectId().toString(), "Susan", 22, "Female", 10000);
         Employee toUpdateEmployee = new Employee(new ObjectId().toString(), "Tom", 23, "Male", 12000);
 
-        when(employeeRepository.findById(employeeId)).thenReturn(employee);
+        when(employeeMongoRepository.findById(employeeId)).thenReturn(Optional.of(employee));
 
         //when
         Employee updatedEmployee = employeeService.update(employeeId, toUpdateEmployee);
 
         //then
-        verify(employeeRepository).findById(employeeId);
+        verify(employeeMongoRepository).findById(employeeId);
         assertThat(updatedEmployee.getAge(), equalTo(23));
         assertThat(updatedEmployee.getSalary(), equalTo(12000));
         assertThat(updatedEmployee.getName(), equalTo("Susan"));
