09e35e5a7faf1c99f81aeb3ce944563736c9c8f7
Marie Chow <chowma3@oocl.com>
2022-12-08 13:36:50 +0800

    feat: use mongodb id

--- src/main/java/com/rest/springbootemployee/advice/ErrorResponse.java
+++ src/main/java/com/rest/springbootemployee/advice/ErrorResponse.java
@@ -1,8 +1,8 @@
 package com.rest.springbootemployee.advice;
 
 public class ErrorResponse {
-    int code;
-    String message;
+    private int code;
+    private String message;
 
     public ErrorResponse(int code, String message) {
         this.code = code;
